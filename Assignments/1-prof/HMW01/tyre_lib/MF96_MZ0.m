40c3c50aebd86e570a7db56563a5a2710de23e58
AleBiCi <alebianchiceriani@gmail.com>
2026-04-13 16:05:03 +0200

    Added prof MZ0 functions

---
+++ Assignments/1-prof/HMW01/tyre_lib/MF96_MZ0.m
@@ -0,0 +1,16 @@
+% Pure self-aligning torque MZ0
+function [mz0] = MF96_MZ0(kappa, alpha, gamma__w, Fz, tyre_data)
+
+ % precode
+
+  [alpha__y,By,Cy,Dy,Ey,SVy]            = MF96_FY0_coeffs(kappa, alpha, gamma__w, Fz, tyre_data);
+   [alpha__r,alpha__t,Br,Dr,Bt,Ct,Dt,Et] = MF96_MZ0_coeffs(kappa, alpha, gamma__w, Fz, tyre_data);
+  
+   fy0 = magic_formula(alpha__y, By, Cy, Dy, Ey, SVy);
+  
+
+ % main code
+
+  mz0 = -Dt .* cos(Ct .* atan(-Bt .* alpha__t + Et .* (Bt .* alpha__t - atan(Bt .* alpha__t)))) .* cos(alpha) .* fy0 + Dr .* ((alpha__r .^ 2 .* Br .^ 2 + 1) .^ (-0.1e1 ./ 0.2e1)) .* cos(alpha);
+  
+ end
